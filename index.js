71b1b6f9f7d155af8efd7d98d8c28ad061c0e537
Rihab_Atwi <rihab112000@gmail.com>
2022-08-27 11:39:29 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
   // Write your code here!
+  if (someValue<=400){
+    return 'This one is on me!';
+  }
+  else if (someValue>400 && someValue<=2000){
+    return 'That will be twenty bucks.';
+  }
+  else if (someValue>2000 && someValue<2500){
+    return 'I will gladly take your thirty bucks.';
+  }
+  else if (someValue>2500){
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  city == "NYC"? console.log('Ok, sounds good.'): console.log("No, go");
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(expression){
   // Write your code here!
+  switch(expression) {
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+    case 'not as generous':
+      return 'Thank you.';
+      break;
+    default:
+      'Bye.';
+  }
 }
